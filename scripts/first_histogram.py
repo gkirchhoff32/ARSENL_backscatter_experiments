3a23ca1512f92940777a116efc8f5a8408d82089
gkirchhoff32 <grant.kirchhoff@colorado.edu>
2022-02-18 12:30:00 -0700

    starting to load data

--- scripts/first_histogram.py
+++ scripts/first_histogram.py
@@ -1,2 +1,10 @@
 import numpy as np
+import os
 
+cwd = os.getcwd()
+data_dir = cwd + r'/../Data'
+fname1 = data_dir + r'/Dev_0_-_2022-02-18_10.48.52.ARSENL'
+
+data = np.loadtxt(fname1, delimiter=',', skiprows=1)
+
+# TODO: np.loadtxt very slow. use pandas
