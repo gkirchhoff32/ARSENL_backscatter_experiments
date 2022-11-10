adjusted evaluation loss script to fix active detector time weighting

scripts/evaluation_high_OD_iterate.py
@@ -38,13 +38,13 @@ dt = 25e-12                   # [s] TCSPC resolution
 ### PARAMETERS ###
 window_bnd = [30e-9, 33e-9]       # [s] Set boundaries for binning to exclude outliers
 exclude_shots = True                     # Set TRUE to exclude data to work with smaller dataset
-max_lsr_num_ref = int(5e3)                   # If set_max_det set to FALSE, include up to certain number of laser shots
+max_lsr_num_ref = int(1e6)                   # If set_max_det set to FALSE, include up to certain number of laser shots
 max_det_num_ref = 2000                       # If set_max_det set to TRUE, include up to a certain number of detections
 set_max_det = False                          # Set TRUE if data limiter is number of detections instead of laser shots.
 deadtime = 25e-9                  # [s] Acquisition deadtime
 use_stop_idx = True               # Set TRUE if you want to use up to the OD value preceding the reference OD
 run_full = True                   # Set TRUE if you want to run the fits against all ODs. Otherwise, it will just load the reference data.
-include_deadtime = False  # Set True to include deadtime in noise model
+include_deadtime = True  # Set True to include deadtime in noise model
 use_poisson_eval = True  # Set TRUE if you want to use the Poisson model for the evaluation loss
 
 # Optimization parameters
@@ -115,7 +115,8 @@ if run_full:
 
         try:
             t_phot_fit_tnsr, t_phot_val_tnsr, t_phot_eval_tnsr,\
-                n_shots_fit, n_shots_val, n_shots_eval = fit.generate_fit_val_eval(flight_time, flight_time_ref, n_shots, n_shots_ref)
+                t_det_lst_fit, t_det_lst_val, \
+                    n_shots_fit, n_shots_val, n_shots_eval = fit.generate_fit_val_eval(flight_time, flight_time_ref, t_det_lst, n_shots, n_shots_ref)
         except:
             ZeroDivisionError
             print('ERROR: Insufficient laser shots... increase the "max_lsr_num" parameter.')
@@ -123,10 +124,12 @@ if run_full:
 
         # Generate "active-ratio histogram" that adjusts the histogram proportionally according to how many bins the detector was "active vs dead"
         if not include_deadtime:
-            active_ratio_hst = torch.ones(len(bin_edges-1))
+            active_ratio_hst_fit = torch.ones(len(bin_edges-1))
+            active_ratio_hst_val = torch.ones(len(bin_edges-1))
         else:
-            active_ratio_hst = fit.deadtime_noise_hist(t_min, t_max, intgrl_N, deadtime, t_det_lst, n_shots)
-        percent_active = torch.sum(active_ratio_hst).item()/len(active_ratio_hst)
+            active_ratio_hst_fit = fit.deadtime_noise_hist(t_min, t_max, intgrl_N, deadtime, t_det_lst_fit, n_shots_fit)
+            active_ratio_hst_val = fit.deadtime_noise_hist(t_min, t_max, intgrl_N, deadtime, t_det_lst_val, n_shots_val)
+        percent_active = torch.sum(active_ratio_hst_fit).item()/len(active_ratio_hst_fit)
         percent_active_lst.append(percent_active)
 
         # Optimization process
@@ -139,7 +142,7 @@ if run_full:
         # Run fit optimizer
         ax, val_loss_arr, eval_loss_arr, \
             fit_rate_fine, coeffs, C_scale_arr = fit.optimize_fit(M_max, M_lst, t_fine, t_phot_fit_tnsr, t_phot_val_tnsr,
-                                                    t_phot_eval_tnsr, active_ratio_hst,
+                                                    t_phot_eval_tnsr, active_ratio_hst_fit, active_ratio_hst_val,
                                                     active_ratio_hst_ref, n_shots_fit, n_shots_val, n_shots_eval,
                                                     learning_rate, rel_step_lim, intgrl_N,
                                                     max_epochs, term_persist)

scripts/generate_sim_data.py
@@ -97,14 +97,14 @@ if __name__ == '__main__':
     dt_sim = 1e-12
 
     tD = 25e-9  # deadtime
-    Nshot = int(1e4)  # number of laser shots
+    Nshot = int(1e6)  # number of laser shots
     wrap_deadtime = True  # wrap deadtime between shots
 
     window_bnd = [26e-9, 34e-9]
 
     laser_pulse_width = 500e-12  # laser pulse width in seconds
     target_time = 31.2e-9
-    target_amplitude = 1e10  # target peak count rate
+    target_amplitude = 1e6  # target peak count rate
     background = 1e4  # background count rate
 
     serialize = True
@@ -124,7 +124,7 @@ if __name__ == '__main__':
                 flight_time=xr.DataArray(flight_time, dims='flight time'),
                 true_flight_time=xr.DataArray(true_flight_time, dims='true flight time'),
                 n_shots=n_shots,
-                t_det_lst=(['det1', 'det2'], t_det_lst),
+                t_det_lst=(['det1'], t_det_lst),
                 t_phot_lst=('true detections', t_phot_lst),
                 target_amplitude=target_amplitude,
                 target_time=target_time,
@@ -142,15 +142,15 @@ if __name__ == '__main__':
         pickle.dump(processed_data, outfile)
         outfile.close()
 
-    phot_arr = np.array(sorted(np.concatenate(t_phot_lst)))
-    plt.figure()
-    plt.stem(phot_arr, np.ones(phot_arr.size))
-    plt.title('Photons')
-
-    cnt_arr = np.array(sorted(np.concatenate(t_det_lst)))
-    plt.figure()
-    plt.stem(cnt_arr, np.ones(cnt_arr.size))
-    plt.title('Detected Photons')
+    # phot_arr = np.array(sorted(np.concatenate(t_phot_lst)))
+    # plt.figure()
+    # plt.stem(phot_arr, np.ones(phot_arr.size))
+    # plt.title('Photons')
+    # 
+    # cnt_arr = np.array(sorted(np.concatenate(t_det_lst)))
+    # plt.figure()
+    # plt.stem(cnt_arr, np.ones(cnt_arr.size))
+    # plt.title('Detected Photons')
 
     # Scaled time-of-flight histogram
     fig = plt.figure()

scripts/library/fit_polynomial_methods.py
@@ -174,7 +174,7 @@ def deadtime_noise_hist(t_min, t_max, intgrl_N, deadtime, t_det_lst, n_shots):
 
     return torch.tensor(active_ratio_hst)
 
-def generate_fit_val_eval(data, data_ref, n_shots, n_shots_ref):
+def generate_fit_val_eval(data, data_ref, t_det_lst, n_shots, n_shots_ref):
     """
     Generates fit, validation, and evaluation data sets for the fitting routine. Recall (1) Fit set: Dataset used to
     generate the fit; (2) Validation set: Independent dataset used to calculate validation loss; and (3) Evaluation set:
@@ -196,6 +196,9 @@ def generate_fit_val_eval(data, data_ref, n_shots, n_shots_ref):
     t_phot_fit = data[:split_value]
     t_phot_val = data[split_value:]
     t_phot_eval = data_ref[:]
+    split_value_det = int(len(t_det_lst) // 2)
+    t_det_lst_fit = t_det_lst[:split_value_det]
+    t_det_lst_val = t_det_lst[split_value_det:]
 
     # Adjust number of laser shots corresponding to fit and val sets
     ratio_fit_split = len(t_phot_fit) / len(data)
@@ -208,12 +211,12 @@ def generate_fit_val_eval(data, data_ref, n_shots, n_shots_ref):
     t_phot_val_tnsr = torch.tensor(t_phot_val.to_numpy())
     t_phot_eval_tnsr = torch.tensor(t_phot_eval.to_numpy())
 
-    return t_phot_fit_tnsr, t_phot_val_tnsr, t_phot_eval_tnsr, n_shots_fit, n_shots_val, n_shots_eval
+    return t_phot_fit_tnsr, t_phot_val_tnsr, t_phot_eval_tnsr, t_det_lst_fit, t_det_lst_val, n_shots_fit, n_shots_val, n_shots_eval
 
 
-def optimize_fit(M_max, M_lst, t_fine, t_phot_fit_tnsr, t_phot_val_tnsr, t_phot_eval_tnsr, active_ratio_hst,
-                active_ratio_hst_ref, n_shots_fit, n_shots_val, n_shots_eval, learning_rate=1e-1, rel_step_lim=1e-8,
-                intgrl_N=10000, max_epochs=400, term_persist=20):
+def optimize_fit(M_max, M_lst, t_fine, t_phot_fit_tnsr, t_phot_val_tnsr, t_phot_eval_tnsr, active_ratio_hst_fit,
+                active_ratio_hst_val, active_ratio_hst_ref, n_shots_fit, n_shots_val, n_shots_eval, learning_rate=1e-1,
+                rel_step_lim=1e-8, intgrl_N=10000, max_epochs=400, term_persist=20):
 
     t_min, t_max = t_fine[0], t_fine[-1]
 
@@ -253,7 +256,7 @@ def optimize_fit(M_max, M_lst, t_fine, t_phot_fit_tnsr, t_phot_val_tnsr, t_phot_
         t_intgrl = cheby_poly(torch.linspace(0, 1, intgrl_N), M)
         while rel_step > rel_step_lim and epoch < max_epochs:
             fit_model.train()
-            pred_fit, integral_fit = fit_model(intgrl_N, active_ratio_hst, t_fit_norm, t_intgrl, cheby=True)
+            pred_fit, integral_fit = fit_model(intgrl_N, active_ratio_hst_fit, t_fit_norm, t_intgrl, cheby=True)
             loss_fit = loss_fn(pred_fit, integral_fit * n_shots_fit)  # add regularization here
             fit_loss_lst += [loss_fit.item()]
 
@@ -274,13 +277,13 @@ def optimize_fit(M_max, M_lst, t_fine, t_phot_fit_tnsr, t_phot_val_tnsr, t_phot_
 
             epoch += 1
 
-        pred_mod_seg, __ = fit_model(intgrl_N, active_ratio_hst, torch.tensor(t_fine), t_intgrl, cheby=False)
+        pred_mod_seg, __ = fit_model(intgrl_N, active_ratio_hst_fit, torch.tensor(t_fine), t_intgrl, cheby=False)
         fit_rate_fine[M, :] = pred_mod_seg.detach().numpy().T
         coeffs[M, 0:M + 1] = fit_model.C.detach().numpy().T
 
         # Calculate validation loss
         # Using fit generated from fit set, calculate loss when applied to validation set
-        pred_val, integral_val = fit_model(intgrl_N, active_ratio_hst, t_val_norm, t_intgrl, cheby=True)
+        pred_val, integral_val = fit_model(intgrl_N, active_ratio_hst_val, t_val_norm, t_intgrl, cheby=True)
         loss_val = loss_fn(pred_val, integral_val * n_shots_val)
         val_loss_arr[M] = loss_val
 