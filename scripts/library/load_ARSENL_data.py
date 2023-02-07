90194ff8641274aa255416090f7808a0dcfce9bf
gkirchhoff32 <grant.kirchhoff@colorado.edu>
2023-02-06 19:08:08 -0700

    plotted new data

--- scripts/library/load_ARSENL_data.py
+++ scripts/library/load_ARSENL_data.py
@@ -25,8 +25,8 @@ import pickle
 # Path settings
 # NOTE: User should check these settings when running "load_ARSENL_data.py" or "plot_histogram.py"
 cwd = os.getcwd()
-data_dir = r'C:\Users\Grant\OneDrive - UCB-O365\ARSENL\Experiments\SPCM\SPCM_Data_2023.01.31'
-fname = r'\OD50_Dev_0_-_2023-01-31_16.43.48_OD5.0.ARSENL'
+data_dir = r'C:\Users\Grant\OneDrive - UCB-O365\ARSENL\Experiments\SPCM\SPCM_Data_2023.02.06'
+fname = r'\Dev_0_-_2023-02-06_17.51.44_OD3.8.ARSENL'
 picklename = 'pickle.dat'
 create_csv = False
 
