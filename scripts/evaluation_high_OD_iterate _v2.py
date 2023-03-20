4874e109a53bcd3731965b6e0872d6d42c0fa0bf
unknown <gkirchhoff32@gmail.com>
2023-03-20 09:37:37 -0600

    plotted nshots 5e5

--- scripts/evaluation_high_OD_iterate _v2.py
+++ scripts/evaluation_high_OD_iterate _v2.py
@@ -38,7 +38,7 @@ c = 2.99792458e8  # [m/s] Speed of light
 ### PARAMETERS ###
 exclude_shots = True  # Set TRUE to exclude data to work with smaller dataset (enables 'max_lsr_num_fit_ref' variables)
 max_lsr_num_ref = int(6e6)  # Maximum number of laser shots for the reference dataset
-max_lsr_num_fit = int(1e5)  # Maximum number of laser shots for the fit dataset
+max_lsr_num_fit = int(5e5)  # Maximum number of laser shots for the fit dataset
 use_final_idx = True  # Set TRUE if you want to use up to the OD value preceding the reference OD
 start_idx = 1  # If 'use_final_idx' FALSE, set the min idx value to this value (for troubleshooting purposes)
 stop_idx = 2  # If 'use_final_idx' FALSE, set the max+1 idx value to this value (for troubleshooting purposes)
